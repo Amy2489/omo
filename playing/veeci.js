25520eebc184e66bb58373194e6e667984d0f790
Amy <zubairua471@gmail.com>
2025-01-27 18:52:44 +0100

    Initial commit - added website files

---
+++ playing/veeci.js
@@ -0,0 +1,10 @@
+// console.log('hello world');
+// let amy='amina';
+// amy='ogechi';
+// console.log(amy);
+let Amy= {
+    name:'Amina',
+    age:4
+}
+Amy['name']='ogechi';
+console.log(Amy['name'])
