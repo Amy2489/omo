d256fe4d191451d32f79d14a00eb601151f6522d
Amy2489 <amyogechi@yahoo.com>
2025-02-27 07:43:23 +0100

    stuff

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 let cartQuantity = 0;
 let button = document.getElementById('button');
 let button1 = document.getElementById('button1');
-// let button2 = document.getElementById('button2');
 let button3 = document.getElementById('button3');
 
 button.addEventListener('click', function () {
@@ -11,16 +10,10 @@ button.addEventListener('click', function () {
     document.getElementById('demo').innerHTML = cartQuantity;
     alert(`You Cart: ${cartQuantity} `)
 })
-
 button1.addEventListener('click', function () {
     cartQuantity = cartQuantity + 1 ;
     document.getElementById('demo').innerHTML = cartQuantity;
 })
-// button2.addEventListener('click', function () {
-//     cartQuantity = cartQuantity + 1;
-//     document.getElementById('demo').innerHTML = cartQuantity;
-// })
-
 button3.addEventListener('click', function () {
     cartQuantity = 0;
     document.getElementById('demo').innerHTML = cartQuantity;
@@ -29,8 +22,9 @@ button.style.height ='40px'
 button1.style.height ='40px'
 button3.style.height ='40px'
 
-// login and signup buttons
 
+
+// login and signup buttons
 let login = document.getElementById('login');
 let user = document.getElementById('user')
 user.addEventListener('click', () => {
